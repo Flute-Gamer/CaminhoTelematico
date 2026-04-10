d4afa7240eac92bcf81cc1843cf0da4ac982b6aa
Igor Souza Lima <igorsouzalima01@gmail.com>
2026-04-09 23:21:44 -0300

    Implemented OSC in project

---
+++ oscreceivermine.gd
@@ -0,0 +1,12 @@
+extends OSCReceiver
+
+func _process(delta):
+	if target_server.incoming_messages.has(osc_address):
+		var msg = target_server.incoming_messages[osc_address]
+
+		if msg.size() > 0:
+			var valor:int = int(msg[0])
+			print("Valor recebido:", valor)
+
+		# evita repetir a mesma mensagem
+		##target_server.incoming_messages.erase(osc_address)
