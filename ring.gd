a914494c2accdfae352d17123ffdf30874da3efc
Igor Souza Lima <igorsouzalima01@gmail.com>
2026-05-03 17:55:08 -0300

    substituted rings

--- ring.gd
+++ ring.gd
@@ -2,9 +2,15 @@ extends MeshInstance3D
 var speed = 0.02
 var Parent
 var isVisible = true
+var turnRight = true
 
 func _ready() -> void:
 	Parent = get_parent()
+	if self.scale.x < 1:
+		return
+	else:
+		turnRight = false
+		return
 
 func _process(delta: float) -> void:
 	self.visible = isVisible
@@ -14,8 +20,13 @@ func _process(delta: float) -> void:
 	speed = Parent.speed
 	if speed < 0.1:
 		speed = 0.1
-	rotation.x += speed * delta
-	
-	##scale = Vector3(speed*0.2, 0.33, speed*0.2)
-	##if scale < Vector3(0.15, 0.15, 0.15):
-		##scale = Vector3(0.15, 0.33, 0.15)
+	if turnRight:
+		rotation.x += speed/4 * delta
+		scale = Vector3(speed*0.15, speed*0.15, speed*0.15)
+		if scale < Vector3(0.15, 0.15, 0.15):
+			scale = Vector3(0.15, 0.15, 0.15)
+	else:
+		rotation.x += 1.618 * -speed/4 * delta
+		scale = Vector3(speed*0.3, speed*0.3, speed*0.3)
+		if scale < Vector3(0.3, 0.3, 0.3):
+			scale = Vector3(0.3, 0.3, 0.3)
